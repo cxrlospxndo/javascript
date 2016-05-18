50ae5f8265d27ddf112b622e00d2153de6dbbceb
Carlos Pando <carlos.pando@waggl.it>
2016-05-18 15:41:09 -0500

    create repository :+1

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hola")
+
+//get: document.getElementById("mensaje").innerHTML
+//set: document.getElementById("mensaje").innerHTML=
+
+// input
+// document.getElementById("question-nombre").value
+// document.getElementById("question-address").value
+
+// appendChild
